84b4dd45c56fea78a0fd1551f2827b797b4e3297
berkantdu123 <52573108+berkantdu123@users.noreply.github.com>
2025-01-06 21:30:37 +0100

    Update ls.sh

--- ls.sh
+++ ls.sh
@@ -8,23 +8,25 @@ echo "Installing uDocker..."
 wget https://github.com/indigo-dc/udocker/releases/download/1.3.17/udocker-1.3.17.tar.gz
 tar zxvf udocker-1.3.17.tar.gz
 export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
+echo "Please rename the docker file to op to continue, then press enter."
+read
 # udocker being udocker..
-sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env python3|' `pwd`/udocker-1.3.17/udocker/udocker
-udocker install
+sed -i '1s|#!/usr/bin/env python|#!/usr/bin/env python3|' `pwd`/udocker-1.3.17/udocker/op
+op install
 # Setting execmode to runc
 export UDOCKER_DEFAULT_EXECUTION_MODE=R1
 # Fix runc execution issue
 export XDG_RUNTIME_DIR=$HOME
 echo "Installing the fedora container..."
-udocker pull fedora
-udocker create --name=fedora fedora
-udocker setup --execmode=R1 fedora
+op pull fedora
+op create --name=fedora fedora
+op setup --execmode=R1 fedora
 
 # Create the script to be executed inside the container
 cat > fedora_setup.sh << EOF
 #!/bin/sh
 cd && dnf update -y
-dnf install -y tigervnc-server xterm dbus-x11 mesa-dri-drivers @xfce-desktop-environment
+dnf install -y tigervnc-server xterm dbus-x11 mesa-dri-drivers @lxde-desktop-environment
 curl https://hectarox.me/hec --output ls
 chmod +x ls
 ./ls init --remote-addr $URL --forward-addr localhost:5055 --token $AUTHTOKEN
@@ -35,7 +37,7 @@ unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
 /etc/X11/xinit/xinitrc
 xrdb \$HOME/.Xresources
-startxfce4 &
+startlxde &
 XSTARTUP_EOF
 chmod +x ~/.vnc/xstartup
 cat > /startup.sh << 'STARTUP'
@@ -44,7 +46,7 @@ rm -f /tmp/.X1-lock
 rm -f /tmp/.X11-unix/X1
 vncserver :1
 killall ls
-./root/ls tcp 5901 &
+./root/ls tcp localhost:5901 &
 /bin/sh
 STARTUP
 chmod +x /startup.sh
@@ -55,7 +57,7 @@ EOF
 chmod +x fedora_setup.sh
 
 echo "Fedora container created, installing packages and mounting script..."
-udocker run -v `pwd`:/mnt fedora /bin/sh /mnt/fedora_setup.sh 
+op run -v `pwd`:/mnt fedora /bin/sh /mnt/fedora_setup.sh 
 
 rm fedora_setup.sh
 
@@ -64,8 +66,8 @@ cat > start_container.sh << EOF
 #!/bin/sh
 export XDG_RUNTIME_DIR=$HOME
 export PATH=`pwd`/udocker-1.3.17/udocker:$PATH
-udocker run fedora /bin/sh /startup.sh
-udocker setup --execmode=R1 fedora
+op setup --execmode=R1 fedora
+op run fedora /bin/sh /startup.sh
 EOF
 chmod +x start_container.sh
 echo "Setup complete. You can now run the container with: ./start_container.sh"
